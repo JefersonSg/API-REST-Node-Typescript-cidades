4bc03da0d10cc6a71fb00d492532739b052c5eb7
JefersonSg <jefersongabri@gmail.com>
2023-10-27 08:47:29 -0300

    feat: added code styles configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
-
 const server = express();
 
+interface teste {
+
+}
+
 server.get('/', (req, res) => {
-  return res.send('Olá, DEVV!')
-})
-
-
+  return res.send('Olá, DEVV!');
+});
 
 export { server };
